632c5646e1babf6112d4642a1c8cfba0784826a2
PHCodes <47927305+PHCodes@users.noreply.github.com>
2019-02-23 23:09:38 -0500

    Updates

---
+++ precache-manifest.bb0719505d88f3d71cbdf6535b4eac71.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "703146e3dfaf828cbcd153c4b5c725a2",
+    "url": "/tarot-site/static/media/Calypso.703146e3.jpg"
+  },
+  {
+    "revision": "24d9279b18be34b47849",
+    "url": "/tarot-site/static/css/main.c35fb19c.chunk.css"
+  },
+  {
+    "revision": "39a484de778c07a3d4b2",
+    "url": "/tarot-site/static/js/1.39a484de.chunk.js"
+  },
+  {
+    "revision": "c1f43ad186c5d9e84f3b",
+    "url": "/tarot-site/static/js/runtime~main.c1f43ad1.js"
+  },
+  {
+    "revision": "f9bb28eaf2d27aad8347f32d071168a5",
+    "url": "/tarot-site/static/media/Mana.f9bb28ea.jpg"
+  },
+  {
+    "revision": "69051024bc15396fcc312caf09f14c81",
+    "url": "/tarot-site/static/media/Mana2.69051024.gif"
+  },
+  {
+    "revision": "2f8a163ef6499258b9142a492c7e010a",
+    "url": "/tarot-site/static/media/Adamant.2f8a163e.jpg"
+  },
+  {
+    "revision": "d94c40f7fdec6797156f54f152c9c03f",
+    "url": "/tarot-site/static/media/Adamant2.d94c40f7.jpg"
+  },
+  {
+    "revision": "c6953ef4ec4c909e06dbcedab00204b8",
+    "url": "/tarot-site/static/media/Sofia.c6953ef4.PNG"
+  },
+  {
+    "revision": "f3bd1ef586e9c282cc60ac40962da6b7",
+    "url": "/tarot-site/static/media/Sofia2.f3bd1ef5.gif"
+  },
+  {
+    "revision": "24d9279b18be34b47849",
+    "url": "/tarot-site/static/js/main.24d9279b.chunk.js"
+  },
+  {
+    "revision": "58abb264678022a64fbcd7f31d65efc9",
+    "url": "/tarot-site/static/media/Calypso2.58abb264.PNG"
+  },
+  {
+    "revision": "53d97b2f820f768523e9d70ad8054a28",
+    "url": "/tarot-site/static/media/Henrex.53d97b2f.png"
+  },
+  {
+    "revision": "f953767c957f65b2f46c811589265702",
+    "url": "/tarot-site/static/media/Henrex2.f953767c.png"
+  },
+  {
+    "revision": "13aa38c5ca707953d7abc73bd25119d9",
+    "url": "/tarot-site/static/media/Yasuko.13aa38c5.PNG"
+  },
+  {
+    "revision": "0ab00e0c4f7528fa4a178a491d5bf2fb",
+    "url": "/tarot-site/static/media/Yasuko2.0ab00e0c.jpg"
+  },
+  {
+    "revision": "afb06c122f3f665cf5eab41eb2cbc42f",
+    "url": "/tarot-site/static/media/Suzu.afb06c12.PNG"
+  },
+  {
+    "revision": "4a3e426237f9f93fbc022db3cea4a974",
+    "url": "/tarot-site/static/media/Suzu2.4a3e4262.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tarot-site/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c059ba3978916ad034970fade4fa16d9",
+    "url": "/tarot-site/index.html"
+  }
+];
